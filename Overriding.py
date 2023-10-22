28a4d55ea8c867c2e383c7770c238cfe75e69d75
Aditiy-Iyer <aditiy.aiyer@gmail.com>
2023-10-22 19:52:04 +0530

    Overriding change

---
+++ Overriding.py
@@ -0,0 +1,12 @@
+class Employee:
+   def func_message(self):
+        print('Welcome to Employee')
+class Department(Employee):
+     def func_message(self):
+        print('Welcome to Department!')
+        print('This is inherited from Employee')
+emp = Employee()
+emp.func_message()
+print('------------')
+dept = Department()
+dept.func_message()
