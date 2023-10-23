8a1a71cc8574a583fb8721ae3f07a5e09d6b66d8
Aditiy-Iyer <aditiy.aiyer@gmail.com>
2023-10-23 10:20:46 +0530

    Overloading Modified

--- Overloading.py
+++ Overloading.py
@@ -1,8 +1,10 @@
 class MyClass:
-   def my_method(self, arg1, arg2=None):
-      if arg2:
+   def my_method(self, arg1, arg2=None, arg3 = None):
+      if arg3:
+         print(arg1 + arg2 + arg3)
+      elif arg2:
          print(arg1 + arg2)
-      else:
+      elif arg1:
          print(arg1)
 
 # Calling the method with one argument
@@ -11,3 +13,5 @@ obj.my_method(10)
 
 # Calling the method with two arguments
 obj.my_method(10, 20) 
+# Calling the method with three arguments
+obj.my_method(10, 20, 30) 
