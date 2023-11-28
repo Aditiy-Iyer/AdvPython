7a95241c44996d3e357257b3c25df64301b04b03
aditiy.aiyer@gmail.com <aditiy.aiyer@gmail.com>
2023-11-29 01:13:41 +0530

    flask

---
+++ flask_login.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return redirect('/login')
+
+@app.route('/login')
+def server_login():
+    return render_template("login.html")
+
+@app.route('/login_process', methods = ["POST", "GET"])
+def get_form_data():
+    return jsonify({"username": request.form.get("username"), "password": request.form.get("password")})
+
+if __name__ == '__main__':
+    app.run(debug=True)
